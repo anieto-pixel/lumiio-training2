96de4f79dd86ded619909075a6373bf63533fc6c
Anieto <71115722+anieto-pixel@users.noreply.github.com>
2022-01-24 18:18:24 -0800

    Update web.php

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -20,16 +20,6 @@ Route::get('/', function () {
 Route::get('/', function () {
     return view('posts');
 });
-/*as a second set of value arguments, this method accepts
-        an array of key-value pairs, in the variable "post*, that in this case we'll use
-        to ensure that the text in the
-        posts displayed aren't always the same one*/
-Route :: get('posts/{slug}', function($slug){/* the braces is one of the things that didnt work in this version
- here mean somethign like"and whatever
- is in them behave slike a wild card, and is the variable slug; would it matter what we write inside
- the braces though?, how does it know it goes to slug now?*/
-    $post = file_get_contents(__DIR__ . "/../resources/posts/{$slug}}.html");
-        return view ('post',['post'=>$post]);
-        /*  complicated version of
-           'post' => file_get_contents($filename);/*
-});
+
+
+
